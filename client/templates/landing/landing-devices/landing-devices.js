4f18f20d7581d06a4ecf50fcbca04419da98c9d9
Omid Ahourai <oahourai@gmail.com>
2015-11-23 15:01:38 -0800

    svg icons, social footer

---
+++ client/templates/landing/landing-devices/landing-devices.js
@@ -0,0 +1,11 @@
+Template.landingDevices.onRendered(function() {
+
+});
+
+Template.landingDevices.helpers({
+	
+});
+
+Template.landingDevices.events({
+  
+});
--- client/templates/landing/landing-footer/landing-footer.js
+++ client/templates/landing/landing-footer/landing-footer.js
@@ -1,11 +1,24 @@
-Template.landingFooter.onRendered(function() {
+Template.landingFooter.onCreated(function() {
 
+});
+
+Template.landingFooter.onRendered(function() {
+	this.$('.over').css('display', 'none');
 });
 
 Template.landingFooter.helpers({
-	
+
 });
 
 Template.landingFooter.events({
-  
+	'mouseenter .icon': function(e) {
+		var target = $(e.target);
+		target.find('.up').css('display', 'none');
+		target.find('.over').css('display', 'block');
+	},
+	'mouseleave .icon': function(e) {
+		var target = $(e.target);
+		target.find('.up').css('display', 'block');
+		target.find('.over').css('display', 'none');
+	},
 });
