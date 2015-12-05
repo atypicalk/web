7ed20f8bfef69a81e51b13ae939ed50ffb3fc6b2
Omid Ahourai <oahourai@gmail.com>
2015-12-05 14:33:06 -0800

    privacy policy

--- both/router/routes.js
+++ both/router/routes.js
@@ -11,41 +11,49 @@ if (Meteor.isCordova) {
 		name: 'landing',
 		controller: 'LandingController'
 	});
-	
+
 	// Profile Route
 	Router.route('/profile', {
 		name: 'profile',
 		controller: 'ProfileController'
 	});
-	
+
+	// Profile Route
+	Router.route('/privacypolicy', {
+		name: 'privacypolicy',
+		onAfterAction: function () {
+			window.scrollTo(0, 0);
+		}
+	});
+
 	// User Route
 	Router.route('/user/:_id', {
 		name: 'userPage',
 		data: function() {
-      var userId = this.params._id;
-      return Meteor.users.findOne({ _id: userId });
-    }
+			var userId = this.params._id;
+			return Meteor.users.findOne({ _id: userId });
+		}
 	});
 
 	// News Feed route
 	Router.route('/newsfeed', {
-    name: 'newsfeed',
-    onBeforeAction: function(){
-      var currentUser = Meteor.userId();
-      if(currentUser) {
-        this.next();
-      } else {
-        this.render("login");
-      }
-    }
+		name: 'newsfeed',
+		onBeforeAction: function(){
+			var currentUser = Meteor.userId();
+			if(currentUser) {
+				this.next();
+			} else {
+				this.render("login");
+			}
+		}
 	});
 
-  // Admin
+	// Admin
 	Router.route('/admin', {
 		name: 'admin',
 		controller: 'AdminController'
 	});
-
+	
 	// Dashboard route
 	// Router.route('/dashboard', {
 	// 	name: 'dashboard',
@@ -68,4 +76,3 @@ if (Meteor.isCordova) {
 // Router.plugin('ensureSignedIn', {
 //   only: ['profile', 'admin']
 // });
-
